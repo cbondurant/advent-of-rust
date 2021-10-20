c31b887feea8619435d8b1754162514de638186c
Conner Bondurant <bondurantc2@berea.edu>
2021-10-20 15:27:22 -0400

    change calling convention

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,11 @@ fn get_input(year: i32, day: i32) -> String{
     read_to_string(Path::new(&format!("inputs/{}-{}.txt", year, day))).expect("")
 }
 
-fn puzzle_2015_01(input : &str) -> i64{
+fn puzzle_2015_1() -> (i64, i64){
+    let input = get_input(2015, 1);
     let mut floor = 0;
     let mut count = 0;
+    let mut basecount = 0;
     for c in input.chars(){
         count += 1;
         match c {
@@ -16,13 +18,13 @@ fn puzzle_2015_01(input : &str) -> i64{
             '\n' => (),
             _ => panic!("Invalid Input!")
         }
-        if floor == -1 {
-          break
+        if (floor == -1)  & (basecount == 0) {
+          basecount = count
         }
     }
-    count
+    (floor, basecount)
 }
 
 fn main() {
-    println!("{}", puzzle_2015_01(&get_input(2015,01)));
+    println!("{:?}", puzzle_2015_1());
 }
