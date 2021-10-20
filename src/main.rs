74eeb6b64481150157f59adea5885025bd499834
Conner Bondurant <bondurantc2@berea.edu>
2021-10-20 16:41:59 -0400

    Solve 2015012

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::fs::read_to_string;
 use std::path::Path;
-use std::cmp::min;
+use std::cmp::{min,max};
 
 fn get_input(year: i32, day: i32) -> String{
     read_to_string(Path::new(&format!("inputs/{}-{}.txt", year, day))).expect("")
@@ -28,18 +28,25 @@ fn puzzle_2015_1() -> (i64, i64){
 
 fn puzzle_2015_2() -> (i64, i64){
     let input = get_input(2015, 2);
-    let mut total = 0;
+    let mut total_area = 0;
+    let mut total_ribbon = 0;
     for line in input.split("\n") {
         let items: Vec<i64> = line.split("x").map(|e| e.parse::<i64>().expect("Invalid Input!")).collect();
         if items.len() == 3 {
             let xy = items[0] * items[1];
             let yz = items[1] * items[2];
             let xz = items[2] * items[0];
-            let smallest = min(min(xy,yz),xz);
-            total += xy*2 + yz*2 + xz*2 + smallest;
+            let xp = (items[0])*2;
+            let yp = (items[1])*2;
+            let zp = (items[2])*2;
+            let smallest_p = xp+yp+zp - max(max(xp,yp),zp);
+            let bow = items[0] * items[1] * items[2];
+            let smallest_a = min(min(xy,yz),xz);
+            total_area += xy*2 + yz*2 + xz*2 + smallest_a;
+            total_ribbon += bow + smallest_p
         }
     }
-    (total, 0)
+    (total_area, total_ribbon)
 }
 
 fn main() {
