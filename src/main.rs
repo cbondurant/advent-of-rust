333feb1ebd3aa3f102120fba182a8ad72d354a2b
Conner Bondurant <bondurantc2@berea.edu>
2021-10-20 13:11:16 -0400

    18012 star obtained

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,20 @@ fn get_input(year: i32, day: i32) -> String{
 
 fn puzzle_2018_01(input : &str) -> i64{
     let mut floor = 0;
+    let mut count = 0;
     for c in input.chars(){
+        count += 1;
         match c {
             '(' => floor += 1,
             ')' => floor -= 1,
             '\n' => (),
             _ => panic!("Invalid Input!")
         }
+        if floor == -1 {
+          break
+        }
     }
-    floor
+    count
 }
 
 fn main() {
