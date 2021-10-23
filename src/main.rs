25dd8f4e4cab89e7da34cb07400beaf47b9ca1a7
Conner Bondurant <conner.bondurant@triptych.me>
2021-10-23 12:11:05 -0400

    Solve 2015081 2015082

--- src/main.rs
+++ src/main.rs
@@ -108,10 +108,10 @@ fn puzzle_2015_4() -> (i64, i64) {
     loop {
         let attempt = format!("{}{}", input, i);
         // gotta force it to the right type
-        //        let hash = md5::compute(attempt);
-        //        if format!("{:x}", hash).starts_with("000000"){
-        break;
-        //        }
+        let hash = md5::compute(attempt);
+        if format!("{:x}", hash).starts_with("000000") {
+            break;
+        }
         i += 1;
     }
     (fivezeroes, i)
@@ -223,6 +223,50 @@ fn puzzle_2015_6() -> (i64, i64) {
     (numon, dimmerlights.iter().sum())
 }
 
+fn puzzle_2015_8() -> (i64, i64) {
+    let input = get_input(2015, 8);
+    //let input = "\"\\x27\"\n\"aaa\\\"aaa\"\n\"\"\n\"abc\"";
+    let mut total_chars = 0;
+    let mut mem_chars = 0;
+    let mut escape_chars = 0;
+    for line in input.split("\n") {
+        if line == "" {
+            break;
+        }
+        total_chars += line.len();
+        escape_chars += line.len() + 2; // open and close "
+        let mut chars = line.chars();
+        while let Some(c) = chars.next() {
+            if c == '"' {
+                escape_chars += 1; // '"' to '\"'
+                continue;
+            }
+            mem_chars += 1;
+
+            if c == '\\' {
+                escape_chars += 1; // '\' to '\\'
+                let c = chars.next().unwrap();
+                if c == 'x' {
+                    chars.nth(1); // consume 2 values
+                }
+
+                if c == '\\' {
+                    escape_chars += 1;
+                }
+
+                if c == '"' {
+                    escape_chars += 1;
+                }
+                continue;
+            }
+        }
+    }
+    (
+        (total_chars - mem_chars) as i64,
+        (escape_chars - total_chars) as i64,
+    )
+}
+
 fn main() {
-    println!("{:?}", d1507::puzzle_2015_7());
+    println!("{:?}", puzzle_2015_8());
 }
